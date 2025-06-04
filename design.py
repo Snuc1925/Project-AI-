b326f74f44e2faaf577d31af7a29c5263baef52d
tonhulily <tonhu170605@gmail.com>
2025-06-04 23:04:30 +0700

    Update design.py

--- design.py
+++ design.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from tkinter import *
+import os
 from tkinter import messagebox
-from main import start_display
+from test_main_easy import start_display as start_display_easy
+from MCTS import start_display as start_display_medium
+from test_main_hard import start_display as start_display_hard
 
 class DotAndBoxGame:
     def __init__(self, root):
@@ -9,9 +12,14 @@ class DotAndBoxGame:
         self.root.title("Dots and Boxes")
         self.root.geometry("500x500+1000+300")
         self.root.resizable(False, False)
-        self.root.iconbitmap("Images\\dotsandboxes.ico")
+        icon_path = os.path.join(os.path.dirname(__file__), 'images', 'dotsandboxes.ico')
+        self.root.iconbitmap(icon_path)
 
         self.selected_mode = None
+        self.selected_level = None
+        self.selected_ai1 = None
+        self.selected_ai2 = None
+        
         self.create_main_menu()
 
     def clear_window(self):
@@ -26,63 +34,150 @@ class DotAndBoxGame:
 
         container.columnconfigure(0, weight=1)
 
-        container.rowconfigure(0, weight=2)  
-        container.rowconfigure(1, weight=1) 
-        container.rowconfigure(2, weight=1) 
-        container.rowconfigure(3, weight=1)  
-        container.rowconfigure(4, weight=2)  
+        container.rowconfigure(0, weight=2)
+        container.rowconfigure(1, weight=1)
+        container.rowconfigure(2, weight=1)
+        container.rowconfigure(3, weight=1)
+        container.rowconfigure(4, weight=2)
 
         title = tk.Label(container, text="Dots and Boxes", fg="red", font=("Comfortaa", 30))
         title.grid(row=0, column=0)
 
-        mode = tk.Label(container, text = "Chọn chế độ:", font = ("VNI-Dom",18))
-        mode.grid(row = 1,column = 0)
+        mode = tk.Label(container, text="Chọn chế độ:", font=("VNI-Dom", 18))
+        mode.grid(row=1, column=0)
 
-        btn_person_vs_ai = tk.Button(container, text="Person vs AI", font=("VNI-Dom", 14), command=lambda: self.set_mode("Person vs AI"), bd="4", bg="light blue",fg="purple", width="10", pady="5")
+        btn_person_vs_ai = tk.Button(container, text="Person vs AI", font=("VNI-Dom", 14),
+                                     command=lambda: self.set_mode("Person vs AI"),
+                                     bd="4", bg="light blue", fg="purple", width="10", pady="5")
         btn_person_vs_ai.grid(row=2, column=0)
 
-        btn_ai_vs_ai = tk.Button(container, text="AI vs AI", font=("VNI-Dom", 14), command=lambda: self.set_mode("AI vs AI"), bd="4", bg="light green",fg="darkred", width="10", pady="5")
+        btn_ai_vs_ai = tk.Button(container, text="AI vs AI", font=("VNI-Dom", 14),
+                                 command=lambda: self.set_mode("AI vs AI"),
+                                 bd="4", bg="light green", fg="darkred", width="10", pady="5")
         btn_ai_vs_ai.grid(row=3, column=0)
-        
-    def set_mode(self,mode):
+
+    def set_mode(self, mode):
         self.selected_mode = mode
-        if (mode == "Person vs AI"): self.select_level()
-        else: self.select_board_size()
-    
+        if mode == "Person vs AI":
+            self.select_level()
+        elif mode == "AI vs AI":
+            self.select_ai1()
+        else:
+            self.select_board_size()
+
+    def set_level_and_continue(self, level):
+        self.selected_level = level
+        self.select_board_size()
+
+    def blink_level5(self):
+        if self.level5_btn and self.level5_btn.winfo_exists():
+            current_color = self.level5_btn.cget("fg")
+            next_color = "gray" if current_color == "white" else "white"
+            self.level5_btn.config(fg=next_color)
+            self.root.after(500, self.blink_level5)
+
     def select_level(self):
         self.clear_window()
         container = tk.Frame(self.root)
         container.pack(expand=True, fill="both")
-
         container.columnconfigure(0, weight=1)
+        for i in range(6):
+            container.rowconfigure(i, weight=1)
 
-        container.rowconfigure(0, weight=2)  
-        container.rowconfigure(1, weight=1) 
-        container.rowconfigure(2, weight=1) 
-        container.rowconfigure(3, weight=1)  
-        container.rowconfigure(4, weight=1)
-        container.rowconfigure(5, weight=2) 
-        
-        
         label = tk.Label(container, text="Chọn độ khó:", font=("VNI-Dom", 16))
-        # label.pack(pady=30)
-        label.grid(row=1,column=0)
+        label.grid(row=0, column=0)
 
-        btn_easy = tk.Button(container, text="Dễ", font=("VNI-Dom", 14), command=self.select_board_size, bd="6", bg="light pink",fg="blue")
-        # btn_easy.pack(pady=20)
-        btn_easy.grid(row=2,column=0)
+        levels = [
+            ("Level 1", "green", "white"),
+            ("Level 2", "yellow", "black"),
+            ("Level 3", "#ff9999", "blue"),
+            ("Level 4", "#cc0000", "white")
+        ]
 
-        btn_medium = tk.Button(container, text="Vừa", font=("VNI-Dom", 14), command=self.select_board_size, bd="6", bg="light pink",fg="blue")
-        # btn_hard.pack()
-        btn_medium.grid(row=3,column=0)
-        
-        btn_hard = tk.Button(container, text="Khó", font=("VNI-Dom", 14), command=self.select_board_size, bd="6", bg="light pink",fg="blue")
-        # btn_hard.pack()
-        btn_hard.grid(row=4,column=0)
+        for i, (level, bg_color, fg_color) in enumerate(levels):
+            btn = tk.Button(
+                container, text=level, font=("VNI-Dom", 14),
+                bg=bg_color, fg=fg_color, bd=5,
+                command=lambda lvl=level: self.set_level_and_continue(lvl)
+            )
+            btn.grid(row=i+1, column=0, pady=2)
+
+        # Level 5 with blinking effect
+        self.level5_btn = tk.Button(
+            container, text="Level 5", font=("VNI-Dom", 14),
+            bg="#800080", fg="white", bd=5,
+            command=lambda: self.set_level_and_continue("Level 5")
+        )
+        self.level5_btn.grid(row=5, column=0, pady=2)
+        self.blink_level5()
 
         btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.create_main_menu)
         btn_back.pack(side=tk.BOTTOM, pady=10)
-    
+        
+    def select_ai1(self):
+        self.clear_window()
+
+        container = tk.Frame(self.root)
+        container.pack(expand=True, fill="both")
+
+        container.columnconfigure(0, weight=1)
+        for i in range(7):
+            container.rowconfigure(i, weight=1)
+
+        label = tk.Label(container, text="Chọn AI1:", font=("VNI-Dom", 16))
+        label.grid(row=0, column=0)
+
+        ai_options = [
+            ("Random", "#8CFF9E"),                # xanh nhạt
+            ("Intuitive Algorithm","#FFEB99"),   # vàng nhạt
+            ("Genetic Algorithm","#FFA07A"),     # cam nhạt
+            ("Alpha-Beta", "#FF6666"),            # đỏ nhạt
+            ("MCTS", "#9933FF")                  # tím đậm
+        ]
+
+        for i, (ai_name,color) in enumerate(ai_options):
+            btn = tk.Button(container, text=ai_name, font=("VNI-Dom", 14), bd=5,
+                            command=lambda ai=ai_name: self.set_ai1(ai), width=15, bg=color)
+            btn.grid(row=i+1, column=0, pady=2)
+
+        btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.create_main_menu)
+        btn_back.pack(side=tk.BOTTOM, pady=10)
+
+    def set_ai1(self, ai_name):
+        self.selected_ai1 = ai_name
+        self.select_ai2()
+
+    def select_ai2(self):
+        self.clear_window()
+
+        container = tk.Frame(self.root)
+        container.pack(expand=True, fill="both")
+
+        container.columnconfigure(0, weight=1)
+        for i in range(7):
+            container.rowconfigure(i, weight=1)
+
+        label = tk.Label(container, text="Chọn AI2:", font=("VNI-Dom", 16))
+        label.grid(row=0, column=0)
+
+        ai_options = [
+            ("Random", "#8CFF9E"),                # xanh nhạt
+            ("Intuitive Algorithm","#FFEB99"),   # vàng nhạt
+            ("Genetic Algorithm","#FFA07A"),     # cam nhạt
+            ("Alpha-Beta", "#FF6666"),            # đỏ nhạt
+            ("MCTS", "#9933FF")                  # tím đậm
+        ]
+        for i, (ai_name, color) in enumerate(ai_options):
+            btn = tk.Button(container, text=ai_name, font=("VNI-Dom", 14), bd=5,bg=color,
+                            width=15, command=lambda ai=ai_name: self.set_ai2(ai))
+            btn.grid(row=i+1, column=0, pady=2)
+
+        btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.select_ai1)
+        btn_back.pack(side=tk.BOTTOM, pady=10)
+
+    def set_ai2(self, ai_name):
+        self.selected_ai2 = ai_name
+        self.select_board_size()
 
     def select_board_size(self):
         self.clear_window()
@@ -131,24 +226,47 @@ class DotAndBoxGame:
                 blink()  # Hiệu ứng nhấp nháy
             # btn.pack(pady=20)
 
-        btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.select_level)
+        if self.selected_mode == "AI vs AI":
+            btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=lambda: self.select_ai2(self.selected_ai1))
+        elif self.selected_mode == "Person vs AI":
+            btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.select_level)
+        else:
+            btn_back = tk.Button(self.root, text="← Quay lại", font=("VNI-Dom", 10), command=self.create_main_menu)
         btn_back.pack(side=tk.BOTTOM, pady=10)
-        
+
     def initiate_game_start(self, board_size_str, mode):
-        """Hides the Tkinter window and calls the actual game start function."""
         try:
-            self.root.withdraw() # Hide the Tkinter window
-            start_display(board_size_str, mode, self.on_back_to_menu) # Call the function in main.py
+            self.root.withdraw()
+            if board_size_str == "3x3":
+                real_board_size = "4x4"
+            elif board_size_str == "4x4":
+                real_board_size = "5x5"
+            elif board_size_str == "4x5":
+                real_board_size = "5x6"
+            elif board_size_str == "5x5":
+                real_board_size = "6x6"
+            else:
+                real_board_size = board_size_str
+            if mode == "Person vs AI":
+                if self.selected_level == "Level 1":
+                    start_display_easy(real_board_size, mode, self.on_back_to_menu)
+                elif self.selected_level == "Level 2":
+                    start_display_medium(real_board_size, mode, self.on_back_to_menu)
+                elif self.selected_level == "Level 3":
+                    start_display_hard(real_board_size, mode, self.on_back_to_menu)
+                else:
+                    raise ValueError("Chưa chọn mức độ khó.")
+            else:
+                start_display_easy(real_board_size, mode, self.on_back_to_menu)
         except Exception as e:
             messagebox.showerror("Error", f"Could not start game: {e}")
         finally:
             try:
-                 if self.root.winfo_exists():
+                if self.root.winfo_exists():
                     self.root.deiconify()
                     self.create_main_menu()
             except tk.TclError:
-                 print("Tkinter window was closed.")
-                 
+                print("Tkinter window was closed.")
     def on_back_to_menu(self):
         self.root.deiconify()
         self.create_main_menu()
@@ -158,4 +276,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = DotAndBoxGame(root)
     root.mainloop()
-
